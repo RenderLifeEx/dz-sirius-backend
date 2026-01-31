d5543d65b4995eb6b6e2e110ddaa8a263814947f
Eugene Koshelev <renderlife@mail.ru>
2026-01-31 20:53:20 +0300

    Скопировал тестовый проект

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'sirius-dz-backend',
+      script: 'pnpm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3003
+      },
+    },
+  ],
+};
